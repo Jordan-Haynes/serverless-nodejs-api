5aa5cdfc1decb87f1a98d2fcabe64849fef9a5ff
Jordan Haynes <jwhaynes101@gmail.com>
2024-07-19 18:04:30 -0400

    First initial setup of packages and Express handler

---
+++ src/handler.js
@@ -0,0 +1,32 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+    DEBUG: process.env.DEBUG === 1 || `${process.env.DEBUG}` === `1`,
+    DATABASE_URL: process.env.DATABASE_URL
+      ? process.env.DATABASE_URL
+      : "not here",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// server-full app
+/* app.listen(3000, () => {
+  console.log("running at http://localhost:3000");
+}); */
+
+exports.handler = serverless(app);
